3b354a574e8861648cd3298bfaa095aa99b111e7
Calvin Rodrigues <calvinrodrigues500@gmail.com>
2021-05-16 13:55:16 +0530

    Corrected sliver appbar in profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -13,14 +13,14 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   List<String> imageList = [
-    'https://cdn.pixabay.com/photo/2019/03/15/09/49/girl-4056684_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
-    'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
-    'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
     'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
     'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
     'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
     'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2019/03/15/09/49/girl-4056684_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
+    'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
+    'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
     'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
     'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
     'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
@@ -34,12 +34,19 @@ class _ProfilePageState extends State<ProfilePage> {
           child: CustomScrollView(
             slivers: [
               SliverAppBar(
+                elevation: 0,
                 floating: true,
-                backgroundColor: Colors.red,
-                expandedHeight: height * 0.4,
-                leading: Icon(
-                  Icons.arrow_back,
-                  color: Colors.black,
+                pinned: true,
+                backgroundColor: Colors.white,
+                expandedHeight: height * 0.43,
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
                 ),
                 actions: [
                   Icon(
@@ -47,6 +54,140 @@ class _ProfilePageState extends State<ProfilePage> {
                     color: Colors.black,
                   ),
                 ],
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                    margin: EdgeInsets.only(top: 40.0),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.white,
+                            Color(0xFFF17FBA).withOpacity(0.2),
+                          ]),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(25.0),
+                          bottomRight: Radius.circular(25.0)),
+                      // boxShadow: [
+                      //   BoxShadow(
+                      //       color: Colors.grey.withOpacity(0.1),
+                      //       blurRadius: 12.0,
+                      //       spreadRadius: 3.0,
+                      //       offset: Offset(0.0, 20.0))
+                      // ]
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'My Profile',
+                            style: GoogleFonts.poppins(
+                                fontSize: 22.0, fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Center(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage: AssetImage(
+                                      'assets/images/main profile.jpg'),
+                                  radius: 40.0,
+                                ),
+                                SizedBox(
+                                  height: 20.0,
+                                ),
+                                Text(
+                                  'Anna Alvarado',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  'Guildhall School of Music & Drama\nLondon, UK',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w200),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(
+                                  height: 20.0,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 25.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        children: [
+                                          Text(
+                                            'Photos',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w300),
+                                          ),
+                                          Text(
+                                            '456',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Text(
+                                            'Followers',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w300),
+                                          ),
+                                          Text(
+                                            '602',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Text(
+                                            'Follows',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w300),
+                                          ),
+                                          Text(
+                                            '290',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
               buildImageView(height)
             ],
@@ -55,42 +196,39 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget buildImageView(var height) => SliverToBoxAdapter(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Container(
-              height: height * 2,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Container(
-                  height: height * 0.5,
-                  child: StaggeredGridView.countBuilder(
-                    primary: false,
-                    shrinkWrap: true,
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 12.0,
-                    itemCount: imageList.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(15.0)),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15.0),
-                          child: FadeInImage.memoryNetwork(
-                            placeholder: kTransparentImage,
-                            image: imageList[index],
-                            fit: BoxFit.cover,
-                          ),
+        child: Container(
+            margin: EdgeInsets.only(top: 20.0),
+            height: height * 2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Container(
+                height: height * 0.5,
+                child: StaggeredGridView.countBuilder(
+                  physics: BouncingScrollPhysics(),
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 12.0,
+                  itemCount: imageList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(15.0)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child: FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: imageList[index],
+                          fit: BoxFit.cover,
                         ),
-                      );
-                    },
-                    staggeredTileBuilder: (index) {
-                      return StaggeredTile.count(1, index.isEven ? 1.2 : 1.8);
-                    },
-                  ),
+                      ),
+                    );
+                  },
+                  staggeredTileBuilder: (index) {
+                    return StaggeredTile.count(1, index.isEven ? 1.2 : 1.8);
+                  },
                 ),
-              )),
-        ),
+              ),
+            )),
       );
 }
